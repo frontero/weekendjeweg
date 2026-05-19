53ed164fafa8e4c35a6a47f807b61294f465e439
Robert van Dijk <32875079+frontero@users.noreply.github.com>
2026-05-19 13:36:09 +0200

    Add stay detail placeholder view

---
+++ src/views/StayDetailView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <section class="view-section" aria-labelledby="detail-title">
+      <p class="view-section__eyebrow">Verblijf</p>
+      <h1 id="detail-title">Detailpagina in voorbereiding</h1>
+      <p class="view-section__intro">
+        Deze route is alvast beschikbaar zodat de catalogus en detailervaring in latere bolts kunnen aansluiten.
+      </p>
+
+      <RouterLink
+        :to="{ name: 'browse' }"
+        class="text-link"
+      >
+        Terug naar ontdekken
+      </RouterLink>
+    </section>
+  </div>
+</template>
