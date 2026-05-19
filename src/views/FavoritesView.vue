d3b56ec1cf1779d0e7fab5109eb83ef5d9c4fb13
Robert van Dijk <32875079+frontero@users.noreply.github.com>
2026-05-19 13:36:11 +0200

    Add favorites placeholder view

---
+++ src/views/FavoritesView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <section class="view-section" aria-labelledby="favorites-title">
+      <p class="view-section__eyebrow">Bewaard</p>
+      <h1 id="favorites-title">Favorieten komen eraan</h1>
+      <p class="view-section__intro">
+        In Bolt 004 voegen we favoriete verblijven toe met toegankelijke toggles en duidelijke status.
+      </p>
+
+      <RouterLink
+        :to="{ name: 'browse' }"
+        class="text-link"
+      >
+        Bekijk verblijven
+      </RouterLink>
+    </section>
+  </div>
+</template>
