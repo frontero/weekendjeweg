b9d71356e6331919046c6d3d96aeb0c142d0882b
Robert van Dijk <32875079+frontero@users.noreply.github.com>
2026-05-19 15:13:30 +0200

    Add Nuxt app shell

---
+++ app/app.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <a
+      class="skip-link"
+      href="#main-content"
+    >
+      Naar hoofdinhoud
+    </a>
+
+    <header class="site-header">
+      <NuxtLink
+        class="brand-link"
+        to="/"
+      >
+        Weekendjeweg
+      </NuxtLink>
+
+      <nav
+        class="site-nav"
+        aria-label="Hoofdnavigatie"
+      >
+        <NuxtLink
+          class="site-nav__link"
+          to="/parken"
+        >
+          Parken
+        </NuxtLink>
+        <NuxtLink
+          class="site-nav__link"
+          to="/regio/nederland"
+        >
+          Regio's
+        </NuxtLink>
+      </nav>
+    </header>
+
+    <main
+      id="main-content"
+      tabindex="-1"
+    >
+      <NuxtPage />
+    </main>
+  </div>
+</template>
