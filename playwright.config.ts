66cd6b0adb881be41314bc3fac5fc7a9917e1f1e
Robert van Dijk <32875079+frontero@users.noreply.github.com>
2026-05-19 15:15:30 +0200

    Update Playwright for Nuxt preview

--- playwright.config.ts
+++ playwright.config.ts
@@ -7,12 +7,12 @@ export default defineConfig({
   retries: process.env.CI === 'true' ? 2 : 0,
   reporter: 'html',
   use: {
-    baseURL: 'http://127.0.0.1:4173',
+    baseURL: 'http://127.0.0.1:3000',
     trace: 'on-first-retry',
   },
   webServer: {
-    command: 'npm run preview -- --host 127.0.0.1',
-    url: 'http://127.0.0.1:4173',
+    command: 'npm run preview -- --host 127.0.0.1 --port 3000',
+    url: 'http://127.0.0.1:3000',
     reuseExistingServer: process.env.CI === 'true' ? false : true,
   },
   projects: [
