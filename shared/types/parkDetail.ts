f55e2a459696c246eb68890aaaf0223b816f2232
Robert van Dijk <32875079+frontero@users.noreply.github.com>
2026-05-20 14:31:38 +0200

    Add park detail content types

---
+++ shared/types/parkDetail.ts
@@ -0,0 +1,23 @@
+export interface ParkGalleryImage {
+  url: string
+  altText: string
+}
+
+export interface ParkFacilityStory {
+  eyebrow: string
+  title: string
+  description: string
+  imageUrl: string
+  imageAltText: string
+}
+
+export interface ParkTestimonial {
+  quote: string
+  author: string
+  context: string
+}
+
+export interface StructuredDataScript {
+  type: 'application/ld+json'
+  innerHTML: string
+}
